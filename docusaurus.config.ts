7554c56ac4d78e63908fa97b16ad40b1963247b0
Bogdan Mihai Nicolae <bmnicolae@gmail.com>
2025-01-22 06:03:29 +0200

    Updated config for webtoknow

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,20 +5,20 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
+  title: 'Web to know',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://webtoknow.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'webtoknow', // Usually your GitHub org/user name.
+  projectName: 'website', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -37,6 +37,7 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
+          routeBasePath: '/projects',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
@@ -64,13 +65,23 @@ const config: Config = {
     ],
   ],
 
+  headTags: [
+    {
+      tagName: 'link',
+      attributes: {
+        href: 'https://fonts.googleapis.com/css2?family=Urbanist:wght@600..800&display=swap',
+        rel: 'stylesheet',
+      },
+    },
+  ],
+
   themeConfig: {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'Home',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'Web to know logo',
         src: 'img/logo.svg',
       },
       items: [
@@ -78,11 +89,12 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Projects',
         },
         {to: '/blog', label: 'Blog', position: 'left'},
+        {to: '/about', label: 'About us', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/webtoknow/website',
           label: 'GitHub',
           position: 'right',
         },
@@ -92,46 +104,54 @@ const config: Config = {
       style: 'dark',
       links: [
         {
-          title: 'Docs',
+          title: 'Useful links',
           items: [
             {
-              label: 'Tutorial',
-              to: '/docs/intro',
+              label: 'Project',
+              to: '/projects/intro',
             },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'X',
-              href: 'https://x.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
             {
               label: 'Blog',
               to: '/blog',
             },
             {
+              label: 'About us',
+              to: '/about',
+            }
+          ],
+        },
+        {
+          title: 'Other Projects',
+          items: [
+            {
+              label: 'CSS Press',
+              href: 'https://webtoknow.github.io/CSS-Press/',
+            },
+            {
+              label: 'Human in Learning',
+              href: 'https://humansinlearning.ro/',
+            },
+            {
+              label: 'Finance Computing Master',
+              href: 'https://financial-computing.upb.ro/',
+            },
+          ],
+        },
+        {
+          title: 'Folow us',
+          items: [
+            {
+              label: 'Youtube',
+              href: 'https://www.youtube.com/watch?v=_hJXIVPQhDo',
+            },
+            {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/webtoknow',
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Web to know`,
     },
     prism: {
       theme: prismThemes.github,
