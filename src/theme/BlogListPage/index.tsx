e2de87c9e593afca40b037b1252037d8677a4267
Bogdan Mihai Nicolae <bmnicolae@gmail.com>
2025-02-06 06:09:12 +0200

    Remove unused blog post page hook from BlogListPageContent

--- src/theme/BlogListPage/index.tsx
+++ src/theme/BlogListPage/index.tsx
@@ -32,7 +32,6 @@ function BlogListPageMetadata(props: Props): ReactNode {
 
 function BlogListPageContent(props: Props): ReactNode {
   const {metadata, items, sidebar} = props;
-  // const {isBlogPostPage} = useBlogPost();
   console.log('props: ', props);
   return (
     <BlogLayout sidebar={sidebar}>
