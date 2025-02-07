c70090ce9c92e5a6e0a0510b3a781ccae4286527
Bogdan Mihai Nicolae <bmnicolae@gmail.com>
2025-02-07 16:30:00 +0200

    Enhance blog details with image

--- src/theme/BlogLayout/index.tsx
+++ src/theme/BlogLayout/index.tsx
@@ -1,29 +1,38 @@
-import React, {useContext, type ReactNode} from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import BlogSidebar from '@theme/BlogSidebar';
+import React, { useContext, type ReactNode } from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import BlogSidebar from "@theme/BlogSidebar";
 
-import type {Props} from '@theme/BlogLayout';
-import { useLocation } from '@docusaurus/router';
+import type { Props } from "@theme/BlogLayout";
+import { useLocation } from "@docusaurus/router";
+
+import styles from './styles.module.css';
 
 export default function BlogLayout(props: Props): ReactNode {
-  const {sidebar, toc, children, ...layoutProps} = props;
-  const {pathname} = useLocation();
-  const isBlogPostDetailsPage = pathname.includes('/blog/');
+  const { sidebar, toc, children, ...layoutProps } = props;
+  const { pathname } = useLocation();
+  const isBlogPostDetailsPage = pathname.includes("/blog/");
 
   return (
     <Layout {...layoutProps}>
       <div className="container margin-vert--lg">
         <div className="row">
-         <BlogSidebar sidebar={sidebar} />
+          <BlogSidebar sidebar={sidebar} />
           <main
-            className={clsx('col', {
-              'col--12': !isBlogPostDetailsPage,
-              'col--9': isBlogPostDetailsPage,
-            })}>
+            className={clsx("col", {
+              "col--12": !isBlogPostDetailsPage,
+              "col--9": isBlogPostDetailsPage,
+            })}
+          >
             {children}
           </main>
-          {toc && <div className="col col--3">{toc}</div>}
+          {toc && (
+            <aside
+              className={clsx("col col--3", isBlogPostDetailsPage && styles.aside)}
+            >
+              {toc}
+            </aside>
+          )}
         </div>
       </div>
     </Layout>
--- src/theme/BlogPostItem/Header/index.tsx
+++ src/theme/BlogPostItem/Header/index.tsx
@@ -3,15 +3,22 @@ import BlogPostItemHeaderTitle from '@theme/BlogPostItem/Header/Title';
 import BlogPostItemHeaderInfo from '@theme/BlogPostItem/Header/Info';
 import BlogPostItemHeaderAuthors from '@theme/BlogPostItem/Header/Authors';
 import { useBlogPost } from '@docusaurus/plugin-content-blog/lib/client/contexts.js';
+import clsx from 'clsx';
+
+import styles from './styles.module.css';
+
 
 export default function BlogPostItemHeader(): ReactNode {
-   const {isBlogPostPage} = useBlogPost();
+   const {isBlogPostPage,  metadata: {
+    frontMatter: { image },
+  },} = useBlogPost();
 
   return (
-    <header>
+    <header className={clsx(isBlogPostPage && styles.header)}>
       <BlogPostItemHeaderTitle />
       <BlogPostItemHeaderInfo />
       {isBlogPostPage && <BlogPostItemHeaderAuthors />}
+      {isBlogPostPage && <img src={image} className={clsx(styles.image)} alt="Blog Image" />}
     </header>
   );
 }
