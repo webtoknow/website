40cfd4ad14d9bfa67d2802fc4e937e8b5ff8ccca
Bogdan Mihai Nicolae <bmnicolae@gmail.com>
2025-03-04 08:01:08 +0200

    Enhance About page layout with new grid structure and icons; add SVG assets for improved visual representation

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -6,7 +6,11 @@ import clsx from "clsx";
 import { useColorMode } from "@docusaurus/theme-common";
 import { DocusaurusConfig } from "@docusaurus/types";
 
-import AboutSvg from './undraw-about.svg';
+import AboutSvg from "./undraw-about.svg";
+import LinkedinSvg from "./linkedin-square.svg";
+import MicrochipSvg from "./microchip.svg";
+import TargetSvg from "./target.svg";
+import ComputerSvg from "./computer-alt.svg";
 
 import styles from "./index.module.css";
 
@@ -36,34 +40,70 @@ export default function About(): ReactNode {
     >
       <main className="container margin-vert--lg">
         <AboutHero siteConfig={siteConfig} />
-        <p>
-          At Webtoknow, we're dedicated to delivering the latest updates on web
-          technologies, IOT, security, and IT jobs. Our blog is your go-to
-          source for in-depth tutorials and informative articles designed to
-          enrich your knowledge in these areas. Our mission is to deliver
-          high-quality, informative content that empowers our audience to
-          navigate the ever-evolving world of technology with confidence.{" "}
-        </p>
-        <p>
-          Whether you're a novice seeking to grasp the fundamentals of web
-          development or a seasoned professional aiming to stay on top of the
-          latest trends, our blog caters to your needs. We cover a diverse range
-          of topics, including popular web development frameworks like React,
-          Angular, and Vue, as well as back-end technologies such as Node.js,
-          Java, and Spring
-        </p>
-        <p>
-          We provide comprehensive tutorials on setting up and utilizing IOT
-          devices, including renowned platforms like Raspberry Pi and Arduino.
-          Our blog also delves into various security topics, offering valuable
-          insights and the latest cybersecurity developments to help safeguard
-          your online presence.
-        </p>
-        <p>
-          We offer a dedicated IT jobs section offers insights into the current
-          job market landscape, featuring the latest job openings and providing
-          valuable tips for job seekers.
-        </p>
+        <article className={clsx(styles.wrapper)}>
+          <header className={clsx(styles.iconWrapper)}>
+            <TargetSvg className={clsx(styles.icon, styles.invertedIcon)} />
+          </header>
+          <section>
+            <h2 className={clsx(styles.title)}>01. Our mission</h2>
+            <p className={clsx(styles.text)}>
+              At Webtoknow, we're dedicated to delivering the latest updates on
+              web technologies, IOT, security, and IT jobs. Our blog is your go-to
+              source for in-depth tutorials and informative articles designed to
+              enrich your knowledge in these areas. Our mission is to deliver
+              high-quality, informative content that empowers our audience to
+              navigate the ever-evolving world of technology with confidence.{" "}
+            </p>
+          </section>
+        </article>
+        <article className={clsx(styles.wrapper, styles.invertedWrapper)}>
+          <header
+            className={clsx(styles.iconWrapper, styles.invertedIconWrapper)}
+          >
+            <ComputerSvg className={clsx(styles.icon)} />
+          </header>
+          <section className={clsx(styles.invertedSection)}>
+            <h2 className={clsx(styles.title)}>02. Web development</h2>
+            <p className={clsx(styles.text)}>
+              Whether you're a novice seeking to grasp the fundamentals of web
+              development or a seasoned professional aiming to stay on top of
+              the latest trends, our blog caters to your needs. We cover a
+              diverse range of topics, including popular web development
+              frameworks like React, Angular, and Vue, as well as back-end
+              technologies such as Node.js, Java, and Spring
+            </p>
+          </section>
+        </article>
+        <article className={clsx(styles.wrapper)}>
+          <header className={clsx(styles.iconWrapper)}>
+            <MicrochipSvg className={clsx(styles.icon, styles.invertedIcon)} />
+          </header>
+          <section>
+            <h2 className={clsx(styles.title)}>03. IOT and security</h2>
+            <p className={clsx(styles.text)}>
+              We provide comprehensive tutorials on setting up and utilizing IOT
+              devices, including renowned platforms like Raspberry Pi and Arduino.
+              Our blog also delves into various security topics, offering valuable
+              insights and the latest cybersecurity developments to help safeguard
+              your online presence.
+            </p>
+          </section>
+        </article>
+        <article className={clsx(styles.wrapper, styles.invertedWrapper)}>
+          <header
+            className={clsx(styles.iconWrapper, styles.invertedIconWrapper)}
+          >
+            <LinkedinSvg className={clsx(styles.icon)} />
+          </header>
+          <section className={clsx(styles.invertedSection)}>
+            <h2 className={clsx(styles.title)}>04. Job Market</h2>
+            <p className={clsx(styles.text)}>
+              We offer a dedicated IT jobs section offers insights into the
+              current job market landscape, featuring the latest job openings and
+              providing valuable tips for job seekers.
+            </p>
+          </section>
+        </article>
 
         <h2>Team</h2>
 
